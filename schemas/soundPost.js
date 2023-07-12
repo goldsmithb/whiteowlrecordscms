36a606c405823afdb5533d4dbebff250c8705369
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2023-07-12 16:07:25 -0400

    create post types

--- schemas/soundPost.js
+++ schemas/soundPost.js
@@ -1,8 +1,8 @@
 import {defineField, defineType} from 'sanity'
 
 export default defineType({
-  name: 'post',
-  title: 'Post',
+  name: 'soundPost',
+  title: 'Sound Post',
   type: 'document',
   fields: [
     defineField({
--- schemas/thoughtPost.js
+++ schemas/thoughtPost.js
@@ -1,8 +1,8 @@
 import {defineField, defineType} from 'sanity'
 
 export default defineType({
-  name: 'post',
-  title: 'Post',
+  name: 'thoughtPost',
+  title: 'Thought Post',
   type: 'document',
   fields: [
     defineField({
