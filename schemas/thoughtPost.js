4309835ab634b6bf3fa1928f6f134d766971df6b
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2023-12-17 13:38:09 -0500

    Update schema to support audio players

--- schemas/thoughtPost.js
+++ schemas/thoughtPost.js
@@ -54,6 +54,48 @@ export default defineType({
       title: 'Body',
       type: 'blockContent',
     }),
+    defineField({
+        name: 'hasAudioPlayer',
+        title: 'Include an optional audio player',
+        type: 'string',
+        options: {
+            list: [
+              {title: 'SoundCloud', value: 'SoundCloud'},
+              {title: 'Bandcamp', value: 'Bandcamp'}
+            ],
+        },
+    }),
+    defineField({
+        name: 'audioPlayerValue',
+        type: 'object',
+        description: 'Add an optional audio player:',
+        hidden: ({document}) => {
+            console.log(document)
+            return !document?.hasAudioPlayer;},
+        fields: [
+            {
+                name: 'soundCloudURL', 
+                type: 'string', 
+                title: 'Sound Cloud URL:',
+                description: 'Simply paste the share URL of the track or album below.',
+                hidden: ({document}) => document?.hasAudioPlayer !== "SoundCloud",
+            },
+            {
+                name: 'bandCampMobile', 
+                type: 'string', 
+                title: 'Bandcamp embedded player (mobile view):',
+                description: 'Navigate to the track or album you would like a player for, and click on "Share / Embed". Select the verticle, condensed option. Paste the "Embed code" below (it will be an HTML iframe element).',
+                hidden: ({document}) => document?.hasAudioPlayer !== "Bandcamp",
+            },
+            {
+                name: 'bandCampWeb', 
+                type: 'string', 
+                title: 'Bandcamp embedded player (desktop view):',
+                description: 'Repeat the steps for a mobile player, but select the large, horizontal option. Paste the "Embed code" below (it will be an HTML iframe element).',
+                hidden: ({document}) => document?.hasAudioPlayer !== "Bandcamp",
+            },
+        ],
+    })
   ],
 
   preview: {
